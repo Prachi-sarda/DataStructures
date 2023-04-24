da74f6db9374e2c75f495e3a5c30e969e1545849
Prachi-sarda <115387396+Prachi-sarda@users.noreply.github.com>
2023-04-24 03:18:30 -0600

    DS Lab 7
    Question 1

---
+++ LabClass7/Question1.c
@@ -0,0 +1,67 @@
+//Write a code in C to evaluate prefix expression
+#include<stdio.h>
+#include<string.h>
+
+int s[50];
+int top=0;
+
+void push(int ch);
+int pop();
+int main()
+{
+	int a,b,c,i;
+	char prefix[50];
+	printf("\nEnter the prefix string in figures: ");
+	gets(prefix);
+	for(i=strlen(prefix)-1;i>=0;i--)
+	{
+		if(prefix[i]=='+')
+		{
+			a=pop();
+            b=pop();
+            c=a+b;
+			push(c);
+		}
+		else if(prefix[i]=='-')
+		{
+			a=pop();
+			b=pop();
+			c=b-a;
+			push(c);
+		}
+		else if(prefix[i]=='*')
+		{	a=pop();
+		b=pop();
+		c=b*a;
+		push(c);
+		}
+		else if(prefix[i]=='/')
+		{
+			a=pop();
+			b=pop();
+			c=b/a;
+			push(c);
+		}
+		else
+		{
+			push(prefix[i]);
+		}
+	}
+	printf("ANSWER = %d",pop());
+
+	return 0;
+}
+
+void push(int ch)
+{
+	top++;
+	s[top]=ch;
+}
+
+int pop()
+{
+	int ch;
+	ch=s[top];
+	top--;
+	return(ch);
+}
